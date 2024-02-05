2b7ca3e739205c9f1eb9f70066698545f778764b
Vikram9392 <vikramvallala12@gmail.com>
2024-02-05 17:33:30 +0530

    done

--- src/app.js
+++ src/app.js
@@ -12,6 +12,20 @@ app.use(express.json())
 
 // GET endpoint for sending the products to client by id
 //Endpoint - /api/v1/names/:id
+app.get("/api/v1/names/:id",(req,res)=>{
+    const {id}=req.params;
+    const productName=productNames.find((entry)=>entry.id==id)
+    if(!productName){
+        return res.status(404).send({status:'failed',message:'Not found!'})
+    }
+    return res.status(200).send({
+        status:"success",
+        message:'Product name fetched successfully',
+        data:{
+            productName
+        }
+    })
+})
 
 
 module.exports = app;
